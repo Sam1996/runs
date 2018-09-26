e0482f6331d234846f5ba6477ce104fa668684da
Sam1996 <samnishanth01@gmail.com>
2018-09-26 18:13:56 -0500

    Initial

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Runs</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light" style="width: 100%">
+				<div class="wraper header-embed">
+					<a class="navbar-brand"></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					    <span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse">
+						<ul class="navbar-nav ml-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Success Stories</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Referral</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link">Blog</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link">Log In</a>
+							</li>
+							<li class="nav-item">
+								<button type="button" class="btn btn-warning">Schedule Your Free Demo</button>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>	
+		</div>
+	</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include_once('header.php'); ?>
+<section class="container-fluid">
+	<div class="row banner">
+		<div class="wraper">
+			<h1 class="lead-text">More Bookings<br>Happier Customers</h1>
+			<p class="lead-text">Online Booking and Reservation Management Software for<br> Tour, Activity, and Rental Operations</p>
+			<button class="btn btn-light btn-lg">Watch Video</button>
+			<button class="btn btn-warning btn-lg">Schedule a Free Demo</button>
+		</div>
+	</div>
+</section>
+<section class="container-fluid">
+	<div class="row">
+		<div class="wraper padding-top-bottom">
+			<h2 class="text-center">Booking Engine is Great For</h2>
+		</div>
+	</div>
+</section>
+<?php include_once('footer.php'); ?>
